09dce0d4cad16a7214c8528c0980c335ec9ebd97
Kurnia Muhamad <omenkzz@gmail.com>
2019-12-31 19:52:45 +0700

    Refactor post controller

--- controllers/posts_controller.go
+++ controllers/posts_controller.go
@@ -2,18 +2,13 @@ package controllers
 
 import (
 	"encoding/json"
-	"net/http"
 	"github.com/omkz/golang-mongo-rest/models"
-	"log"
+	"net/http"
 )
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	payload, err := models.PostAll()
-
-	if err != nil {
-		log.Fatal("Error on Decoding the document", err)
-	}
+	payload, _ := models.PostAll()
 	json.NewEncoder(w).Encode(payload)
 }
